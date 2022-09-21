bdb4f0d9bc5a5725421583bacec01b3ef6e715da
Pavel Rzhevskiy <rzhevskiyp@gmail.com>
2022-09-21 15:59:17 +0700

    Chi25C fit for 2020 Experiment data

--- 4pi_dev/Modelling/start.cpp
+++
@@ -1,6 +0,0 @@
-{
-gROOT->ProcessLine(".L Hypo2ChPionsPi02Photons.cpp++");
-gROOT->ProcessLine(".L TrPh.C++");
-gROOT->ProcessLine("callLoop()");
-
-}
--- install/KFCmd/bin/env.sh
+++
@@ -1,1 +0,0 @@
-export LD_LIBRARY_PATH=/spoolA/przhevskiy/kinfit/install/KFBase/lib:/spoolA/przhevskiy/kinfit/install/KFCmd/lib:$LD_LIBRARY_PATH
